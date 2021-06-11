349e3771b8f858713754a7d3372163e6a0a7a7ac
pauliescanlon <pauliescanlon@gmail.com>
2021-06-11 09:27:10 +0100

    feat: add access control header

--- src/api/github-user.js
+++ src/api/github-user.js
@@ -1,6 +1,7 @@
 const { octokit } = require('../clients')
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   try {
     const user = await octokit.request('GET /user', {
       name: process.env.GATSBY_GITHUB_USERNAME,
--- src/api/twitter-user.js
+++ src/api/twitter-user.js
@@ -1,6 +1,7 @@
 const { twitter } = require('../clients')
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   try {
     const { data } = await twitter.get(`users/by/username/${process.env.GATSBY_TWITTER_USERNAME}`, {
       user: {
@@ -8,7 +9,6 @@ export default async function handler(req, res) {
           'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld',
       },
     })
-
     res.status(200).json({
       user: data,
     })
