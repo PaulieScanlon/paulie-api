ee5370c875c3d4813b0d228928b1c42ce37d282e
pauliescanlon <pauliescanlon@gmail.com>
2021-08-24 22:13:37 +0100

    feat: change env vars

--- src/api/github-user.js
+++ src/api/github-user.js
@@ -1,10 +1,15 @@
 const { octokit } = require('../clients')
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(400).json({ message: 'req.method should be GET' })
+  }
+
   res.setHeader('Access-Control-Allow-Origin', '*')
+
   try {
     const user = await octokit.request('GET /user', {
-      name: process.env.GATSBY_GITHUB_USERNAME,
+      name: process.env.GITHUB_USERNAME,
     })
 
     const repos = await octokit.request('GET /user/repos', {
@@ -17,7 +22,7 @@ export default async function handler(req, res) {
     })
   } catch {
     res.status(500).json({
-      error: 'Ooops server error',
+      error: "Blast! There's been an error",
     })
   }
 }
--- src/api/twitter-user.js
+++ src/api/twitter-user.js
@@ -1,9 +1,14 @@
 const { twitter } = require('../clients')
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(400).json({ message: 'req.method should be GET' })
+  }
+
   res.setHeader('Access-Control-Allow-Origin', '*')
+
   try {
-    const { data } = await twitter.get(`users/by/username/${process.env.GATSBY_TWITTER_USERNAME}`, {
+    const { data } = await twitter.get(`users/by/username/${process.env.TWITTER_USERNAME}`, {
       user: {
         fields:
           'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld',
@@ -14,7 +19,7 @@ export default async function handler(req, res) {
     })
   } catch {
     res.status(500).json({
-      error: 'Ooops server error',
+      error: "Blast! There's been an error",
     })
   }
 }
