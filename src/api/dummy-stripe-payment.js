36ce78a8ed6e5805bcb4272ac0bcc7386b1e2c1a
pauliescanlon <pauliescanlon@gmail.com>
2021-08-29 14:34:57 +0100

    feat: abstract stripe call

--- src/api/dummy-stripe-payment.js
+++ src/api/dummy-stripe-payment.js
@@ -20,26 +20,30 @@ const runCorsMiddleware = async (req, res) => {
   })
 }
 
+const runStripeCheckout = async (req, res) => {
+  await runCorsMiddleware(req, res)
+
+  const session = await stripe.checkout.sessions.create({
+    success_url: success_url,
+    cancel_url: cancel_url,
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          unit_amount: amount * 100,
+          currency: 'usd',
+          product: product,
+        },
+      },
+    ],
+    mode: 'payment',
+  })
+}
+
 export default async function handler(req, res) {
   try {
-    await runCorsMiddleware(req, res)
-
-    const session = await stripe.checkout.sessions.create({
-      success_url: success_url,
-      cancel_url: cancel_url,
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          quantity: 1,
-          price_data: {
-            unit_amount: amount * 100,
-            currency: 'usd',
-            product: product,
-          },
-        },
-      ],
-      mode: 'payment',
-    })
+    runStripeCheckout(req, res)
 
     res.status(200).json({ message: 'A ok!', url: session.url })
   } catch (error) {
