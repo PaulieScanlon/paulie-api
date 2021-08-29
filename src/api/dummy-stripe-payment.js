e9cbb0a492a8d121aae00b550f1b905f6d4a21cd
pauliescanlon <pauliescanlon@gmail.com>
2021-08-29 09:11:35 +0100

    feat: add cors methods

--- src/api/dummy-stripe-payment.js
+++ src/api/dummy-stripe-payment.js
@@ -1,7 +1,9 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 import Cors from 'cors'
 
-const cors = Cors()
+const cors = Cors({
+  methods: ['POST'],
+})
 
 const runCorsMiddleware = async (req, res) => {
   await new Promise((resolve, reject) => {
@@ -9,24 +11,7 @@ const runCorsMiddleware = async (req, res) => {
       if (result instanceof Error) {
         reject(result)
       }
-      resolve(
-        stripe.checkout.sessions.create({
-          success_url: success_url,
-          cancel_url: cancel_url,
-          payment_method_types: ['card'],
-          line_items: [
-            {
-              quantity: 1,
-              price_data: {
-                unit_amount: amount * 100,
-                currency: 'usd',
-                product: product,
-              },
-            },
-          ],
-          mode: 'payment',
-        }),
-      )
+      resolve(result)
     })
   })
 }
@@ -35,22 +20,22 @@ export default async function handler(req, res) {
   try {
     await runCorsMiddleware(req, res)
 
-    // const session = await stripe.checkout.sessions.create({
-    //   success_url: success_url,
-    //   cancel_url: cancel_url,
-    //   payment_method_types: ['card'],
-    //   line_items: [
-    //     {
-    //       quantity: 1,
-    //       price_data: {
-    //         unit_amount: amount * 100,
-    //         currency: 'usd',
-    //         product: product,
-    //       },
-    //     },
-    //   ],
-    //   mode: 'payment',
-    // })
+    const session = await stripe.checkout.sessions.create({
+      success_url: success_url,
+      cancel_url: cancel_url,
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            unit_amount: amount * 100,
+            currency: 'usd',
+            product: product,
+          },
+        },
+      ],
+      mode: 'payment',
+    })
 
     res.status(200).json({ message: 'A ok!', url: session.url })
   } catch (error) {
