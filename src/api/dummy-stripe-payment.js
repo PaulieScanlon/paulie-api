9ce99764eed3214a9942d6cea570f8efeb2bf11a
pauliescanlon <pauliescanlon@gmail.com>
2021-09-01 20:58:33 +0100

    feat: add .com to origin

--- src/api/dummy-stripe-payment.js
+++ src/api/dummy-stripe-payment.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 import Cors from 'cors'
 
 const cors = Cors({
-  origin: 'https://www.mdx-embed',
+  origin: 'https://www.mdx-embed.com',
   methods: ['POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
