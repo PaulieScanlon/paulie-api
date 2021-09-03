a78a239238a97ed43b3d4957de9436c596c0f954
pauliescanlon <pauliescanlon@gmail.com>
2021-09-03 14:04:48 +0100

    feat: nested try catch

--- src/api/make-stripe-payment.js
+++ src/api/make-stripe-payment.js
@@ -29,34 +29,33 @@ export default async function handler(req, res) {
 
   try {
     await runCorsMiddleware(req, res)
+    try {
+      if (!success_url || !cancel_url || !amount || !product) {
+        res.status(400).json({ message: '⚠️ Missing required body params' })
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        success_url: success_url,
+        cancel_url: cancel_url,
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            quantity: 1,
+            price_data: {
+              unit_amount: amount * 100,
+              currency: 'usd',
+              product: product,
+            },
+          },
+        ],
+        mode: 'payment',
+      })
+
+      res.status(200).json({ message: '🕺 Stripe checkout created ok', url: session.url })
+    } catch (error) {
+      res.status(500).json({ message: '🚫 Stripe checkout error' })
+    }
   } catch (error) {
     res.status(500).json({ message: '🚫 Request blocked by CORS' })
-  }
-
-  try {
-    if (!success_url || !cancel_url || !amount || !product) {
-      res.status(400).json({ message: '⚠️ Missing required body params' })
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      success_url: success_url,
-      cancel_url: cancel_url,
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          quantity: 1,
-          price_data: {
-            unit_amount: amount * 100,
-            currency: 'usd',
-            product: product,
-          },
-        },
-      ],
-      mode: 'payment',
-    })
-
-    res.status(200).json({ message: '🕺 Stripe checkout created ok', url: session.url })
-  } catch (error) {
-    res.status(500).json({ message: '🚫 Stripe checkout error' })
   }
 }
