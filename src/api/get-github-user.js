cf799912d081f98e16e3102c31f65f776489d055
pauliescanlon <pauliescanlon@gmail.com>
2021-09-04 17:10:43 +0100

    feat: release v3

---
+++ src/api/get-github-user.js
@@ -0,0 +1,48 @@
+const { octokit } = require('../clients')
+import Cors from 'cors'
+
+const allowedOrigins = []
+
+const cors = Cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error())
+    }
+  },
+})
+
+const runCorsMiddleware = (req, res) => {
+  return new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+  const { username } =
+    typeof req.body === 'string' ? JSON.parse(req.body) : req.body
+
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      await runCorsMiddleware(req, res)
+    }
+
+    try {
+      const { data } = await octokit.request('GET /user', {
+        name: username,
+      })
+
+      res.status(200).json({ message: '🕺 GitHub request ok', user: data })
+    } catch {
+      res.status(500).json({ error: '🚫 GitHub error' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: '🚫 Request blocked by CORS' })
+  }
+}
---
+++ src/test-components/get-github-user.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import axios from 'axios'
+import { Spinner } from 'theme-ui'
+
+const GetGitHubUser = () => {
+  const [response, setResponse] = useState(null)
+
+  const getGitHubUser = async () => {
+    try {
+      const response = await axios('/api/get-github-user', {
+        method: 'POST',
+        data: {
+          username: 'PaulieScanlon',
+        },
+      })
+      setResponse(response.data)
+    } catch (error) {
+      setResponse(error.response)
+    }
+  }
+
+  useEffect(() => {
+    getGitHubUser()
+  }, [])
+
+  return (
+    <Fragment>
+      {response ? (
+        <pre className="language-javascript">
+          {JSON.stringify(response, null, 2)}
+        </pre>
+      ) : (
+        <Spinner />
+      )}
+    </Fragment>
+  )
+}
+
+export default GetGitHubUser
