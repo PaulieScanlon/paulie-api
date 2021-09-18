f70b0240f28009e5a58ee0874035e3428c0605e3
pauliescanlon <pauliescanlon@gmail.com>
2021-09-18 11:08:31 +0100

    chore: temporarily disable CORS for get-github-events

--- src/api/get-github-events.js
+++ src/api/get-github-events.js
@@ -2,6 +2,7 @@ const { octokit } = require('../clients')
 import Cors from 'cors'
 
 const allowedOrigins = [
+  'http://localhost:3000',
   'https://paulieapi.gatsbyjs.io',
   'https://pauliescanlon.github.io',
 ]
@@ -33,7 +34,7 @@ export default async function handler(req, res) {
 
   try {
     if (process.env.NODE_ENV === 'production') {
-      // await runCorsMiddleware(req, res)
+      await runCorsMiddleware(req, res)
     }
 
     try {
