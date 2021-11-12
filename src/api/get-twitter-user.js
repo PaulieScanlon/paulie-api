509904a05075b521234cc05450ec0ee653caa211
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-11-12 10:10:59 +0000

    feat: remove CORS

--- src/api/get-twitter-user.js
+++ src/api/get-twitter-user.js
@@ -3,33 +3,6 @@ import Cors from 'cors'
 import { remark } from 'remark'
 import remarkHtml from 'remark-html'
 
-const allowedOrigins = [
-  'https://paulieapi.gatsbyjs.io',
-  'https://paulie.dev',
-  'https://www.pauliescanlon.io',
-]
-
-const cors = Cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  },
-})
-
-const runCorsMiddleware = (req, res) => {
-  return new Promise((resolve, reject) => {
-    cors(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-      return resolve(result)
-    })
-  })
-}
-
 const convertToMarkdown = async (string) => {
   const response = await remark().use(remarkHtml).process(string)
 
@@ -37,36 +10,31 @@ const convertToMarkdown = async (string) => {
 }
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
   const { username } =
     typeof req.body === 'string' ? JSON.parse(req.body) : req.body
 
   try {
-    if (process.env.NODE_ENV === 'production') {
-      await runCorsMiddleware(req, res)
+    if (!username) {
+      res.status(400).json({ message: '⚠️ Missing required body params' })
     }
-    try {
-      if (!username) {
-        res.status(400).json({ message: '⚠️ Missing required body params' })
-      }
 
-      const { data } = await twitter.get(`users/by/username/${username}`, {
-        user: {
-          fields:
-            'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld',
-        },
-      })
+    const { data } = await twitter.get(`users/by/username/${username}`, {
+      user: {
+        fields:
+          'created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld',
+      },
+    })
 
-      const markdown = await convertToMarkdown(data.description)
+    const markdown = await convertToMarkdown(data.description)
 
-      res.status(200).json({
-        message: '🕺 Twitter request ok',
-        user: data ? data : '🦜 Username not found',
-        markdown: markdown,
-      })
-    } catch {
-      res.status(500).json({ error: '🚫 Twitter error' })
-    }
-  } catch (error) {
-    res.status(403).json({ message: '🚫 Request blocked by CORS' })
+    res.status(200).json({
+      message: '🕺 Twitter request ok',
+      user: data ? data : '🦜 Username not found',
+      markdown: markdown,
+    })
+  } catch {
+    res.status(500).json({ error: '🚫 Twitter error' })
   }
 }
