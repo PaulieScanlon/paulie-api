8a6ffe16c445b17aa5670c75b3371933cbbd7796
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-07-17 11:20:18 +0100

    test: remove cors from get-github-users

--- src/api/get-github-user.js
+++ src/api/get-github-user.js
@@ -1,58 +1,71 @@
 const { octokit } = require('../clients')
-import Cors from 'cors'
+// import Cors from 'cors'
 
-const allowedOrigins = [
-  // undefined allows the GitHub pages build request to succeed
-  undefined,
-  'https://pauliescanlon.github.io',
-  'https://paulieapi.gatsbyjs.io',
-]
+// const allowedOrigins = [
+//   // undefined allows the GitHub pages build request to succeed
+//   undefined,
+//   'https://pauliescanlon.github.io',
+//   'https://paulieapi.gatsbyjs.io',
+// ]
 
-const cors = Cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  },
-})
+// const cors = Cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error())
+//     }
+//   },
+// })
 
-const runCorsMiddleware = (req, res) => {
-  return new Promise((resolve, reject) => {
-    cors(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-      return resolve(result)
-    })
-  })
-}
+// const runCorsMiddleware = (req, res) => {
+//   return new Promise((resolve, reject) => {
+//     cors(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
+//       return resolve(result)
+//     })
+//   })
+// }
 
 export default async function handler(req, res) {
   const { username } =
     typeof req.body === 'string' ? JSON.parse(req.body) : req.body
 
-  // res.setHeader('Access-Control-Allow-Origin', '*') // YOLO
+  res.setHeader('Access-Control-Allow-Origin', '*') // YOLO
 
   try {
-    if (process.env.NODE_ENV === 'production') {
-      await runCorsMiddleware(req, res)
-    }
+    const { data } = await octokit.request(`GET /users/{username}`, {
+      username: username,
+    })
 
-    try {
-      const { data } = await octokit.request(`GET /users/{username}`, {
-        username: username,
-      })
-
-      res.status(200).json({
-        message: '🕺 GitHub request ok',
-        user: data ? data : '🦜 Username not found',
-      })
-    } catch {
-      res.status(500).json({ error: '🚫 GitHub error' })
-    }
-  } catch (error) {
-    res.status(403).json({ message: '🚫 Request blocked by CORS' })
+    res.status(200).json({
+      message: '🕺 GitHub request ok',
+      user: data ? data : '🦜 Username not found',
+    })
+  } catch {
+    res.status(500).json({ error: '🚫 GitHub error' })
   }
+
+  // try {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     await runCorsMiddleware(req, res)
+  //   }
+
+  //   try {
+  //     const { data } = await octokit.request(`GET /users/{username}`, {
+  //       username: username,
+  //     })
+
+  //     res.status(200).json({
+  //       message: '🕺 GitHub request ok',
+  //       user: data ? data : '🦜 Username not found',
+  //     })
+  //   } catch {
+  //     res.status(500).json({ error: '🚫 GitHub error' })
+  //   }
+  // } catch (error) {
+  //   res.status(403).json({ message: '🚫 Request blocked by CORS' })
+  // }
 }
